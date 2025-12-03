d3d3a69199a802f86580792e33b4217dc2b300c0
Ethan McMahon <ethanmcmahon77@gmail.com>
2025-12-03 15:59:22 -0600

    finishing up

--- R/addingfunc.R
+++ R/addingfunc.R
@@ -5,7 +5,7 @@
 #' @returns a table from the csv file
 #' @export
 #'
-#' @examples myread(DDT.csv)
+#' @examples myread(system.file("extdata", "DDT.csv", package = "MATH4753F25EthanM"))
 myread=function(csv){
   fl=paste(csv)
   read.table(fl,header=TRUE,sep=",")
